6a366224b6fa0fd0fb0f5acdcadc6ac4478f2985
whereisjenkins <8914235+whereisjenkins@users.noreply.github.com>
2018-12-16 15:30:39 -0500

    Cleanup
    Removing comments to clean up.

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,7 @@ Route::get('/about', 'PagesController@about') ;
 Route::get('/services', 'PagesController@services') ;
 
 Route::resource('posts', 'PostsController');
-/*
-Route::get('/hello', function(){
-    return '<h1>Hello World</h1>';
-});
 
-Route::get('/about', function(){
-    return view('pages.about');
-});
-
-// Dynamic route
-Route::get('/users/{id}/{name}', function($id, $name){
-    return 'This is user ' . $name . ' with an ID of ' . $id;
-});
-*/
 Auth::routes();
 
 Route::get('/dashboard', 'DashboardController@index');
